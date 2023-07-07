6eeb459833bf117a631df9445b1d1a643bbf2fbf
blessing-quadra <blessing.ladejobi@quadrasystems.org>
2023-07-07 12:23:30 +0100

    removing unwanted directory: pages__old

--- src/pages__old/main/layers/container/ButtonContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ButtonContainer({ children }: { children: JSX.Element }) {
-  return (
-    <div className="w-full flex items-center justify-end py-8 gap-2">{children}</div>
-  );
-}
-
-export default ButtonContainer;
